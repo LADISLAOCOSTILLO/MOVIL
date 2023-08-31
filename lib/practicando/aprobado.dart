720238334db0fefb67e5633cfcb0078023eae2f3
lacoca <032287d@utea.edu.pe>
2023-08-31 11:59:04 -0500

    sentencia if

---
+++ lib/practicando/aprobado.dart
@@ -0,0 +1,17 @@
+void main() {
+
+  double nota1 =10;
+  double nota2 =05;
+  double nota3 =05;
+  
+  double suma=(nota1+nota2+nota3)/3;
+  print("$suma");
+
+  if (suma >=10.5) {
+    print("TU NOTA ES $suma: Y ESTAS APROBADO");
+  }
+  if (suma>=10.5){ 
+    print ("TU NOTA ES $suma: Y  ESTAS DESAPROBADO");
+   } 
+ }
+ 
---
+++ lib/practicando/promedio.dart
@@ -0,0 +1,9 @@
+void main() {
+
+  double nota1 =10.5;
+  double nota2 =15.5;
+  double nota3 =15.2;
+  double promedio =(nota1+nota2+nota3)/3;
+  print("EL PROMEDIO: $promedio");
+ }
+ 
